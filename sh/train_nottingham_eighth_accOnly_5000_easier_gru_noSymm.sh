c415239f731f938f912c2ff4f415a7d9cfb9b820
yichen huang <yh2689@nyu.edu>
2022-11-13 17:20:54 +0400

    sh

--- sh/train_nottingham_eighth_accOnly_5000_easier_gru_noSymm.sh
+++ sh/train_nottingham_eighth_accOnly_5000_easier_gru_noSymm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=nottingham_eighth_accOnly_5000_easier_gru_       # 任务名
+#SBATCH --job-name=nottingham_eighth_accOnly_5000_easier_gru_noSymm       # 任务名
 #SBATCH --nodes=1                   # 这里不用动 多节点脚本请查官方文档
 #SBATCH --ntasks=1                  # 这里不用动 多任务脚本请查官方文档
 #SBATCH --cpus-per-task=4           # 要几块CPU (一般4块就够用了)
@@ -21,12 +21,13 @@ echo "START"               # 输出起始信息
 source deactivate
 source /apps/local/anaconda3/bin/activate danielTrash          # 调用 virtual env
 CUDA_LAUNCH_BLOCKING=1 python -u main_train.py \
-    --name nottingham_eighth_accOnly_5000_2_512_easier_gru__ \
+    --name nottingham_eighth_accOnly_5000_2_512_easier_gru_noSymm \
     --seq_len 64 \
     --data_folder nottingham_eights_pool_accOnly_5000_easier \
     --additional_symm_steps 0 \
     --symm_start_step 0 \
     --rnn_num_layers 2 \
     --rnn_hidden_size 512 \
-    --gru
+    --gru \
+    --no_symm
 echo "FINISH"                       # 输出起始信息
